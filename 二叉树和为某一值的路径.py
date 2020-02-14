1319f0b0e9cdf894d3788af35127b8f5b06eb13a
jiahenghuang <huangjiaheng0901@163.com>
2020-02-14 21:25:44 +0800

    add algorithm

---
+++ 二叉树和为某一值的路径.py
@@ -0,0 +1,16 @@
+#coding:utf-8
+'''
+输入一颗二叉树和一个整数，打印出二叉树中结点值的和为输入整数的所有路径。
+路径定义为从树的根结点开始往下一直到叶结点所经过的结点形成一条路径。
+'''
+
+class Node():
+    def __init__(self, left=None, right=None):
+        self.val=val
+        self.left = left
+        self.right = right
+
+class Solution():
+    def 
+
+
